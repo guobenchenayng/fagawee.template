07d4fd57fa58879071236cda19ec2bb0217dd05c
GitHub Action <action@github.com>
2022-02-18 03:25:50 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/guobenchenayng/fagaweetemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.guobenchenayng.fagaweetemplate.services
+
+import com.github.guobenchenayng.fagaweetemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
